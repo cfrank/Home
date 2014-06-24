5a2de7a79635292eff4783ed42c9431094388d9c
Chris Frank <chris@cfrank.org>
2014-06-24 16:34:53 -0400

    Cross Browser Search
    In oder to make the search feature work on all browser I removed a
    bunch of shit that was breaking it….

--- j/view.js
+++ j/view.js
@@ -15,8 +15,8 @@ $('top').style.background += "url(" + headerImg + ")";
 
 /* Create the html for the search bar */
 var searchHtml = [
-  '<form>',
-  '<input id="homeSearch" type="text" placeholder="Search" autofocus>',
+  '<form method="get" action="https://www.google.com/search">',
+  '<input id="homeSearch" type="text" name="q" placeholder="Search" autofocus>',
   '</form>'
 ].join('');
 
@@ -25,72 +25,22 @@ $('top').innerHTML += searchHtml;
 
 /* HANDLE THE SEARCH */
 
-/* Accepted search providers:
-      - Google
-      - Google Images
-      - Yandex
-      - DuckDuckGo
-      - Wikipedia
-      - YouTube
-*/
-
-/* Select search provider */
-var searchProvider = "Google";
-/*
-    To submit the form the user must press
-    enter, we must grab this keycode (13)
-    and execute the search.
-*/
-
-$('homeSearch').addEventListener('keypress', function(event)
-{
-  if(event.keyCode === 13)
-    {
-      /* Get the content from the field */
-      var searchContent = $('homeSearch').value;
-
-      /*
-          For some reason the form won't redirect
-          to the search provider unless I add a
-          hash to the home page url... I have no idea
-          why this happens. If you know find me in a
-          desktop thread and tell me please ^.^
-      */
-      window.location.hash = "#";
-
-      if(searchProvider === "Google")
-      {
-        window.location = "https://www.google.com/#q=" + searchContent;
-      }else if(searchProvider === "Google Images")
-      {
-        window.location = "https://www.google.com/search?tbm=isch&q=" + searchContent;
-      }else if(searchProvider === "Yandex")
-      {
-        window.location = "http://beta.yandex.ru/yandsearch?lr=110715&text=" + searchContent;
-      }else if(searchProvider === "DuckDuckGo")
-      {
-        window.location = "https://duckduckgo.com/?q=" + searchContent;
-      }else if(searchProvider === "Wikipedia")
-      {
-        window.location = "http://en.wikipedia.org/w/index.php?search=" + searchContent;
-      }else if(searchProvider === "YouTube")
-      {
-        window.location = "https://www.youtube.com/results?search_query=" + searchContent;
-      }
-    }
-});
-
 /* HANDLE THE BOXES */
 
 /* Array with links ["Title", "url"] */
 var links = [
-  ["/g/", "http://boards.4chan.org/g/"],
-  ["/b/", "http://boards.4chan.org/b/"],
   ["/a/", "http://boards.4chan.org/a/"],
-  ["/wg/", "http://boards.4chan.org/wg/"],
+  ["/b/", "http://boards.4chan.org/b/"],
+  ["/g/", "http://boards.4chan.org/g/"],
   ["/r9k/", "http://boards.4chan.org/r9k/"],
-  ["Mail", "https://mail.cock.li"],
+  ["/wg/", "http://boards.4chan.org/wg/"],
+  ["Pomf", "http://pomf.se/"],
   ["YouTube", "https://www.youtube.com"],
+  ["Translate", "https://translate.google.com/"],
+  ["Dribbble", "https://www.dribbble.com"],
+  ["VK", "http://vk.com/mysall"],
+  ["Mail", "https://mail.cock.li"],
+  ["Leddit", "http://www.reddit.com"],
   ["2ch", "http://2ch.hk"],
   ["Stack", "http://stackoverflow.com/"],
   ["Codrops", "http://tympanus.net/codrops/"],
